bd357056295880382e067ac35ea17ef7973d20b0
sammed-21 <sammed2110@gmail.com>
2023-08-22 09:18:22 +0530

    fix align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "../../public/crypto-devs.png";
 import Button from "../components/Button"
 import Link from 'next/link'
 import PageWrapper from "./page-wrapper";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+ 
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 
@@ -21,8 +21,7 @@ export default function Home() {
           <h1 className="text-7xl font-semibold text-transparent text-white  border-none max-md:text-5xl ">
         
           <span className="animate-pulse bg-gradient-to-r from-gray-900 via-gray-800 to-black dark:bg-none dark:text-white bg-clip-text text-transparent">
-              {/* <span className=" animate-pulse bg-gradient-to-r from-gray-900 via-gray-800 to-black bg-clip-text text-transparent"> */}
-                I&apos;m
+                 I&apos;m
                 <br/>
             Sammed S Betadur
               </span>
@@ -51,7 +50,7 @@ export default function Home() {
      
         >
           Contact me {" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          
         </Link>
 
         <a
